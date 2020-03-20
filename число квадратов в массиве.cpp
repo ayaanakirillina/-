794fbb9ac2698147772460421963092bc103bd9c
ayaanakirillina <62022680+ayaanakirillina@users.noreply.github.com>
2020-03-20 11:11:09 +0900

    Add files via upload

---
+++ число квадратов в массиве.cpp
@@ -0,0 +1,18 @@
+//число квадратов в этом массиве
+#include <iostream>
+using namespace std;
+int main() {
+	int n, a[100], b[100], q = 0, i;
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (i = 0; i < n; i++) {
+		b[i] = sqrt(a[i]);
+	}
+	for (i = 0; i < n; i++) {
+		if (pow(b[i], 2) == a[i]) q++;
+	}
+	cout << q << endl;
+	system("pause");
+}
